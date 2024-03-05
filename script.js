3f22dbf1cd57ad42c027c496a1ef38d651d5e5e7
MoeHaz <160973009+MoeHaz@users.noreply.github.com>
2024-03-05 02:24:48 -0500

    buyWeapon function
    I have added the first weapon to this function

--- script.js
+++ script.js
@@ -167,7 +167,12 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    text.innerText = "You now have a new weapon."
+  }
 }
 
 // step 69 
